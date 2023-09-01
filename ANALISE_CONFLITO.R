416fa9807d859222972e5dc245576974d16d1475
Sandro Jk <sandro007@gmail.com>
2023-09-01 18:04:08 -0300

    ok

--- ANALISE_CONFLITO.R
+++ ANALISE_CONFLITO.R
@@ -1,4 +1,5 @@
-## OVERVIEW ACORDOS
+## ANALISE DE CONFLITOS 
+## 01.09.2023
 ## SANDRO JAKOSKA
 
 library(DBI)
@@ -7,75 +8,38 @@ library(tidyverse)
 con3 <- dbConnect(odbc::odbc(), "repro_prod", timeout = 10,encoding="Latin1")
 con2 <- dbConnect(odbc::odbc(), "reproreplica", timeout = 10,encoding="Latin1")
 
-
-
+## QUERIES ==================
 
 df <- dbGetQuery(con2, statement = read_file('ANALISE_CONFLITO_LA.sql'))
 
 View(df)
 
 
-## TABLES =====================================================
+
+comb <- dbGetQuery(con2, statement = read_file('COMBINADOS.sql'))
+
+View(comb)
 
 
-tab_promo <- 
-  dbGetQuery(con2,"
-             WITH PRECO AS (SELECT PROCODIGO,PREPCOVENDA2 FROM PREMP WHERE EMPCODIGO=1),
-             
-             PROD AS (SELECT PROCODIGO,PRODESCRICAO FROM PRODU WHERE PROSITUACAO='A')
-             
-             SELECT T.PROCODIGO, 
-                     PRODESCRICAO,
-                      ROUND(PREPCOVENDA2*(1-TBPPCDESCTO2*1.00/100)*2,2) PROMO FROM TBPPRODU T
-             LEFT JOIN PRECO P ON T.PROCODIGO=P.PROCODIGO
-             LEFT JOIN PROD PR ON T.PROCODIGO=PR.PROCODIGO
-             WHERE TBPCODIGO=1642") 
+pacotes <- dbGetQuery(con2, statement = read_file('PACOTES.sql'))
 
-View(tab_promo)
+View(pacotes)
+
+pacotes %>% group_by(CLICODIGO) %>% tally() %>% View()
 
 
-comb_promo_prod <-dbGetQuery(con3,"
-             WITH TBCOMB AS (SELECT TBPCODIGO FROM TABPRECO 
-             WHERE  TBPTABCOMB='S' AND TBPSITUACAO='A'),
-             
-             PRODPROMO AS (SELECT PROCODIGO FROM TBPPRODU WHERE TBPCODIGO=1642),
-             
-             PRODA AS (SELECT P.PROCODIGO,PRODESCRICAO PROA FROM PRODU P
-             INNER JOIN PRODPROMO PR ON PR.PROCODIGO=P.PROCODIGO
-             WHERE PROSITUACAO='A'),
-             
-             PRODB AS (SELECT PROCODIGO,PRODESCRICAO PROB FROM PRODU WHERE PROSITUACAO='A'),
-             
-             PRECOA AS(SELECT PROCODIGO,PREPCOVENDA2 PRA FROM PREMP WHERE EMPCODIGO=1),
-             
-             PRECOB AS(SELECT PROCODIGO,PREPCOVENDA2 PRB FROM PREMP WHERE EMPCODIGO=1),
-             
-             CLITB AS (SELECT TBPCODIGO,CLICODIGO FROM CLITBPCOMB WHERE CLICODIGO=)
-             
-             SELECT 
-              CLICODIGO,
-             T.TBPCODIGO,
-             PROCODIGOA,
-                     PROA,
-                      CCPCOVENDAPROA2,
-                      CCINDICEPROA2,
-                       PROCODIGOB,
-                        PROB,
-                         CCPCOVENDAPROB,
-                         CCINDICEPROB2, 
-                          ROUND(PRA*(1-CCINDICEPROA2*1.00/100)*2+PRB*(1-CCINDICEPROB2*1.00/100),2) COMBO
-                          FROM TBPCOMBPROPRO T
-             INNER JOIN TBCOMB TB ON TB.TBPCODIGO=T.TBPCODIGO
-              LEFT JOIN PRODA PA ON PA.PROCODIGO=T.PROCODIGOA
-               LEFT JOIN PRODB PB ON PB.PROCODIGO=T.PROCODIGOB
-                LEFT JOIN PRECOA PRA ON PRA.PROCODIGO=T.PROCODIGOA   
-                 LEFT JOIN PRECOB PRB ON PRB.PROCODIGO=T.PROCODIGOB
-                  LEFT JOIN CLITB CT ON CT.TBPCODIGO=T.TBPCODIGO
-                             ") 
+## SET CALC =====================
 
-View(comb_promo_prod)
+conflict_set <- 
+left_join(df,pacotes,by="CLICODIGO")
 
 
-inner_join(tab_promo,tab_promo_comb,by="PROCODIGO") %>% View()
+
+
+
+
+
+
+
 
 
---
+++ BASE_ANALISE.sql
@@ -0,0 +1,37 @@
+WITH CLI AS (
+SELECT DISTINCT C.CLICODIGO,
+                  CLINOMEFANT,
+                   C.GCLCODIGO,
+                     CLIPCDESCPRODU,
+                      GCLNOME,  
+                       SETOR
+                         FROM CLIEN C
+                          INNER JOIN (SELECT CLICODIGO,ZODESCRICAO SETOR FROM ENDCLI E
+                           INNER JOIN (SELECT ZOCODIGO,ZODESCRICAO FROM ZONA WHERE ZOCODIGO IN (20))Z ON 
+                            E.ZOCODIGO=Z.ZOCODIGO WHERE ENDFAT='S')A ON C.CLICODIGO=A.CLICODIGO
+                             LEFT JOIN GRUPOCLI GR ON C.GCLCODIGO=GR.GCLCODIGO
+                              WHERE CLICLIENTE='S'),
+                              
+                              -- PREÇOS
+                               
+                               PRECO AS (SELECT PROCODIGO,PREPCOVENDA2 FROM PREMP WHERE EMPCODIGO=1),
+                               
+                                -- TAB PRECO
+                               
+                                TBPRECO AS (SELECT TBPCODIGO FROM TABPRECO 
+                                 WHERE TBPSITUACAO='A'
+                                  AND (TBPDTVALIDADE=NULL OR TBPDTVALIDADE>='TODAY')),
+                                 
+                                  -- PRODUTO 
+                               
+                                   PROD AS (SELECT TB.PROCODIGO,
+                                             ROUND(PREPCOVENDA2*(1-TBPPCDESCTO2*1.00/100)*2,2) PROMO
+                                              FROM TBPPRODU TB
+                                               LEFT JOIN PRECO PC ON PC.PROCODIGO=TB.PROCODIGO
+                                                WHERE TBPCODIGO=1642 AND TB.PROCODIGO='LA0182')
+                              
+SELECT * FROM CLI
+ CROSS JOIN PROD
+ 
+
+
